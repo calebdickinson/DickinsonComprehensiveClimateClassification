d817843a94ec4be9cf65d6d1b26e2bef6dba6d88
Caleb Dickinson <calebisaacdickinson@gmail.com>
2025-08-03 02:39:39 -0500

    Update SummerMap.js

--- ClimateFiles/SummerMap.js
+++ ClimateFiles/SummerMap.js
@@ -1,33 +1,65 @@
-
-var era5 = ee.ImageCollection('ECMWF/ERA5/MONTHLY')
-  .filterDate('2000-01-01', '2005-12-31')
-  .select('mean_2m_air_temperature')
+// a) NASA/NEX-GDDP
+var data = ee.ImageCollection('NASA/NEX-GDDP')
+  .filter(ee.Filter.eq('scenario', 'historical'))
+  .filter(ee.Filter.calendarRange(1995, 2005, 'year'));
+  
+// Convert tasmax and tasmin from Kelvin to Celsius
+var tasmax = data.select('tasmax')
   .map(function(img) {
-    return img.subtract(273.15).rename('tempC')
-              .set('system:time_start', img.get('system:time_start'));
+    return img
+      .subtract(273.15)
+      .rename('tasmaxC')
+      .copyProperties(img, ['system:time_start']);
+  });
+var tasmin = data.select('tasmin')
+  .map(function(img) {
+    return img
+      .subtract(273.15)
+      .rename('tasminC')
+      .copyProperties(img, ['system:time_start']);
   });
 
+// Build monthly means by averaging tasmax/tasmin
 var months = ee.List.sequence(1, 12);
-
 var monthlyMeans = ee.ImageCollection(
   months.map(function(m) {
-    var mImg = era5.filter(ee.Filter.calendarRange(m, m, 'month')).mean();
-    return mImg.set('month', m).rename('tempC');
+    var maxMean = tasmax
+      .filter(ee.Filter.calendarRange(m, m, 'month'))
+      .mean();
+    var minMean = tasmin
+      .filter(ee.Filter.calendarRange(m, m, 'month'))
+      .mean();
+    // daily‐mean → monthly‐mean
+    return maxMean.add(minMean)
+                  .divide(2)
+                  .rename('monthlyMean')
+                  .set('month', m);
   })
 );
 
-var hottestC_global = monthlyMeans.qualityMosaic('tempC')
-                                  .select('tempC').rename('hottestC');
+// Extract hottest-month and coldest-month rasters
 
+// Hottest‐month: pick the image with the highest monthlyMean at each pixel
+var hottestC_global = monthlyMeans
+  .qualityMosaic('monthlyMean')
+  .select('monthlyMean')
+  .rename('hottestC');
+
+// Coldest‐month: invert, mosaic, then invert back
 var coldestC_global = monthlyMeans
-  .map(function(img){ return img.multiply(-1).copyProperties(img); })
-  .qualityMosaic('tempC')
+  .map(function(img) {
+    return img.multiply(-1).copyProperties(img);
+  })
+  .qualityMosaic('monthlyMean')
   .multiply(-1)
-  .select('tempC').rename('coldestC');
+  .select('monthlyMean')
+  .rename('coldestC');
 
 function classifySummer(tC) {
   return ee.Image.constant(0)
-    .where(tC.gte(40).and(tC.lt(45)),  9) // X1: Extreme Hyperthermal Summer
+    .where(tC.gte(100).and(tC.lt(150)),  11) // Boiling
+    .where(tC.gte(50).and(tC.lt(100)),  10) // Hypercaneal
+    .where(tC.gte(40).and(tC.lt(50)),  9) // X1: Extreme Hyperthermal Summer
     .where(tC.gte(35).and(tC.lt(40)),  8) // Z2: Hyperthermal Summer
     .where(tC.gte(30).and(tC.lt(35)),  7) // Z1: Scorching Hot Summer
     .where(tC.gte(25).and(tC.lt(30)),  6) // A2: Very Hot Summer
@@ -43,6 +75,8 @@ function classifySummer(tC) {
 var warmZone = classifySummer(hottestC_global);
 
 var codeColorMap = {
+  11: "#888888", // Boiling
+  10: "#0000FF", // Hypercaneal
   9: "#000000", // X: Extreme Hyperthermal Summer
   8: "#550000", // Z2: Hyperthermal Summer
   7: "#C71585", // Z1: Scorching Hot Summer
@@ -63,7 +97,7 @@ var indices = codes.map(function(_, i){ return i; });
 
 // 5) Remap → mask → display (one layer only)
 var discreteLand = warmZone
-  .remap(codes, indices, -1)
+  .remap(codes, indices)
   .rename('classIndex');
 
 Map.addLayer(
