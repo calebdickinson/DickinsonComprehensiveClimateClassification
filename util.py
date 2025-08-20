429ebcd769d03a71bd1a306fcc6a767fb5d0b5b5
Hyrum Dickinson <hyrumdickinson@gmail.com>
2025-08-19 20:32:44 -0500

    added climate code maps (data structure)

---
+++ util.py
@@ -0,0 +1,37 @@
+coldest_code = {
+    'H': 'Hypercaneal',
+    'X': 'Uninhabitable',
+    'Z': 'Ultratropical',
+    'A': 'Supertropical',
+    'B': 'Tropical',
+    'C': 'Subtropical',
+    'D': 'Temperate',
+    'E': 'Continental',
+    'F': 'Subarctic',
+    'G': 'Arctic',
+    'Y': 'Superarctic'
+}
+
+aridity_code = {
+    'H': 'Humid',
+    'G': 'Semihumid',
+    'W': 'Monsoon',
+    'M': 'Mediterranean',
+    'S': 'Semiarid',
+    'D': 'Arid Desert'
+}
+
+warmest_code = {
+    'H': 'Hypercaneal Summer',
+    'X': 'Extreme Hyperthermal Summer',
+    'Z2': 'Hyperthermal Summer',
+    'Z1': 'Scorching Hot Summer',
+    'A2': 'Very Hot Summer',
+    'A1': 'Hot Summer',
+    'B2': 'Mild Summer',
+    'B1': 'Cold Summer',
+    'C2': 'Very Cold Summer',
+    'C1': 'Freezing Summer',
+    'Y': 'Frigid Summer',
+
+}
