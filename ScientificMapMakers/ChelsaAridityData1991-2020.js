ae61345ccfc5d083eec852c0f988f954d5fe119d
Caleb Dickinson <calebisaacdickinson@gmail.com>
2025-10-17 04:59:25 -0500

    Update latitude zone thresholds for aridity calculation

--- ScientificMapMakers/ChelsaAridityData1991-2020.js
+++ ScientificMapMakers/ChelsaAridityData1991-2020.js
@@ -61,9 +61,9 @@ var AI      = P_ann.divide(PET_ann).rename('AI');
 
 // Latitude zones (unchanged)
 var lat = ee.Image.pixelLonLat().select('latitude');
-var northMask = lat.gt(5);
-var tropic    = lat.abs().lte(5);
-var southMask = lat.lt(-5);
+var northMask = lat.gt(15);
+var tropic    = lat.abs().lte(15);
+var southMask = lat.lt(-15);
 
 // Base aridity classes (your current thresholds)
 var aridBase = ee.Image(6)
